d2e2db8975cab28bed5c73e44cbed6a2d9bebcdd
TakahashiIkki <new_ittyandesu@outlook.jp>
2017-01-25 23:48:39 +0900

    Study for endEditing Keyboard

---
+++ ControllingTheKeyboard/ControllingTheKeyboard/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  ControllingTheKeyboard
+//
+//  Created by 一騎高橋 on 2017/01/25.
+//  Copyright © 2017年 IkkiTakahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // shutdown キーボード
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+
+}
+
